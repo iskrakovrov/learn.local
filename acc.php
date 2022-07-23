668a99083a4a58d4e21a203af72056c19d85107d
iskra2x <iskrakovrov@gmail.com>
2022-07-23 05:35:33 +0300

    Revert "Исправлено 1"
    This reverts commit d110aab5d072de677988e67f6193807c05ddb39b.

--- acc.php
+++ acc.php
@@ -1,14 +1,12 @@
 <?php
 
-$f = 40923337;
-
-
 include_once('inc/init.php');
 require_once('inc/db.php');
 require_once('function/function.php');
 
 $sql = "SELECT * FROM accounts";
 $query = selectAll($sql);
+
 
 foreach ($query as $a) {
     if (!empty ($a['avatar'])) {
@@ -98,5 +96,3 @@ $data = array(
 // Convert PHP array to JSON array
 $json_data = json_encode($data);
 print $json_data;
-
-
