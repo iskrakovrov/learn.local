60266d87dd597ed6e71d8afa2ede62cef729403d
iskranout <iskrakovrov@gmail.com>
2022-08-11 04:21:43 +0300

    12.08

--- act.php
+++ act.php
@@ -2,6 +2,8 @@
 include_once('inc/init.php');
 require_once('inc/db.php');
 require_once('function/function.php');
+$lang = $_SESSION['lang'] . '.php';
+require_once($lang);
 $red = $_REQUEST['add_task'];
 $ids = $_REQUEST['a'];
 session_start();
