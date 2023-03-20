ee4fb504bc270c009cad663a8589a44e4855dae3
iskrakovrov <iskrakovrov@gmail.com>
2023-03-20 20:21:58 +0200

    20/03

--- test.php
+++ test.php
@@ -1,9 +1,4 @@
-<?php
-include_once('inc/init.php');
-require_once('inc/db.php');
-require_once('function/function.php');
-$lang = $_SESSION['lang'] . '.php';
-require_once($lang);
-$id = $_REQUEST['id'];
-$sql = 'SELECT * FROM friends WHERE id = $id  ORDER BY created DESC LIMIT 20';
-$statFriends = selectAll($sql);
+<script>
+var street = 'улица Зорге';
+var add = number + street;
+</script>
